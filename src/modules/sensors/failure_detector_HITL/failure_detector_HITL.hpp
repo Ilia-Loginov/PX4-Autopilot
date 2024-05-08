7f0751c6b8c5da4900348de656ce700c3322fdb1
Ilia-Loginov <ilia.loginov@tii.ae>
2024-05-08 14:11:25 +0400

    updated the copyright some Hai To's fixes

--- src/modules/sensors/failure_detector_HITL/failure_detector_HITL.hpp
+++ src/modules/sensors/failure_detector_HITL/failure_detector_HITL.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2024 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2024 Technology Innovation Institute. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,9 +32,10 @@
  ****************************************************************************/
 
 /**
- * @file failure_detector_HITL.hpp
+ * @file failure_detector_HITL.cpp
  *
  * @author Ilia Loginov <ilia.loginov@tii.ae>
+ * @author Hai To <hai.to@unikie.com>
  */
 
 #pragma once
