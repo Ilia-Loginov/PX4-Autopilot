1750cbe574d497d3424acf6c52edbbfa49f6304d
Ilia-Loginov <ilia.loginov@tii.ae>
2024-05-08 15:38:56 +0400

    added config for run tests for pixhawk

--- run_gz_pixhawk.sh
+++ run_gz_pixhawk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source Tools/simulation/gazebo-classic/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 gazebo Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds/hitl_iris_eth.world
+
+#run test case
+
+#DONT_RUN=1 make px4_sitl gazebo mavsdk_tests
+#test/mavsdk_tests/mavsdk_test_runner.py test/mavsdk_tests/configs/hitl_eth.json --speed-factor 1 --gui --case 'Takeoff and Land' --verbose --hitl --connection ethernet
