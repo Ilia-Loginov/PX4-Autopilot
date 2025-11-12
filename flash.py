5e47b381fd557d431e6dd8fb8c322ebe0cab2dc4
Ilia-Loginov <ilia.loginov@tii.ae>
2025-11-12 14:06:11 +0400

    Add new airframes for supporting T-Drone-m690 On branch support_hitl_for_release_15_2 Changes to be committed: 	new file:   ROMFS/px4fmu_common/init.d/airframes/1402_t_drone_m690b.hil 	new file:   ROMFS/px4fmu_common/init.d/airframes/4402_t_drone_m690b 	modified:   ROMFS/px4fmu_common/init.d/airframes/CMakeLists.txt 	modified:   ROMFS/px4fmu_common/init.d/rc.hitl_testing 	modified:   boards/px4/fmu-v6x/default.px4board 	new file:   flash.py

---
+++ flash.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+import os
+import subprocess
+
+
+
+def upload_framware(device_path):
+	print("Start upload firmware")
+	script_path = './Tools/px_uploader.py'
+
+	all_script_arguments = [script_path, '--port', device_path, './build/px4_fmu-v6x_default/px4_fmu-v6x_default.px4']
+	command = ["python3"] + all_script_arguments
+
+	try:
+		subprocess.run(command, check=True)
+	except subprocess.CalledProcessError as e:
+		print(f"Error: {e}")
+
+def check_dev(device_path):
+
+        if os.path.exists(device_path):
+            return True
+
+        return False
+
+
+device_pref = "/dev/ttyACM"
+for num in "0", "1":
+	dev_name = device_pref + num
+	if (check_dev(dev_name)):
+		print("Device ", dev_name, " exists")
+		upload_framware(dev_name)
+		break
+
+
